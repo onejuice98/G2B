67abd291cdfe1cb5410ef444fad0241df5b095ae
onejuice98 <waterpurifier@khu.ac.kr>
2023-03-15 21:03:17 +0900

    heroku : 21

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.get("/", async (req, res) => {
   }
   postListObject("20221223", "20220603");
   */
-  res.send("HELLO G2B BOT is Here! v18");
+  res.send("HELLO G2B BOT is Here! v21");
 });
 
 app.listen(port);
